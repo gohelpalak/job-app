f999118628a801566232490b13a515a73f15b622
gohelpalak <159258834+gohelpalak@users.noreply.github.com>
2025-05-20 09:24:14 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/job-app")
+mongoose.connect("mongodb+srv://gohelpalak14:palak%4022@cluster0.cjwxl.mongodb.net/job-app")
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
